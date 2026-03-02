29902258afd57396826b81d38e2fac8ca29dc5b3
vit <leduc071204@gmail.com>
2026-03-02 20:22:52 +0700

    b3 b10

---
+++ BuoiTH3/bai10/10.js
@@ -0,0 +1,15 @@
+function change_background() {
+    document.getElementById("message").style.background = 'red';    
+}
+
+function change_color() {
+    document.getElementById('message').style.color = 'blue';
+}
+
+function change_height(){
+    document.getElementById('message').style.height = '500px';
+}
+
+function change_font_size(){
+    document.getElementById('message').style.fontSize = '500px';
+}
