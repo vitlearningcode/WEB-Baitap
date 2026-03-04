9c0e41238f1d68e96f76e865493761b84fa1bf8a
duck <leduc071204@gmail.com>
2026-03-04 14:34:00 +0700

    b3 bai5

---
+++ BuoiTH3/baitaplamthem/bai5/5.js
@@ -0,0 +1,20 @@
+function show_search_advance() {
+    // Lấy thẻ a và category
+    var a = document.getElementById('show_search_advance');
+    var category = document.getElementById('search_advance');
+    
+    // Lấy nội dung của thẻ a
+    var text = a.innerHTML;
+
+    // Kiểm tra xử lý hiển thị và ẩn + đổi nội dung thẻ a
+    if (text === "Tìm kiếm nâng cao") {
+        category.style.display = "table-row";
+        a.innerHTML = "Bỏ tìm kiếm nâng cao";
+    } else {
+        category.style.display = "none";
+        a.innerHTML = "Tìm kiếm nâng cao";
+    }
+    
+    // Return false để khi click vào thẻ a sẽ không bị chuyển trang
+    return false;
+}
